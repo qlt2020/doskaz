46c9368bb9b9f3e188da6aab4a98f0965d649549
amanum <45536476+amanum@users.noreply.github.com>
2021-10-08 16:14:08 +0600

    closed #125

--- adminpanel/pages/index/objects/index.vue
+++ adminpanel/pages/index/objects/index.vue
@@ -5,41 +5,46 @@
         :table-fields="fields"
         :actions="actions"
     >
-        <objects-filter slot="filter"/>
+        <objects-filter slot="filter" />
     </crud-list>
 </template>
 
 <script>
-    import CrudList from "~/components/crud/CrudList";
-    import FormattedDate from "~/components/crud/list-fields/FormattedDate";
-    import EditLink from "~/components/crud/list-fields/EditLink";
-    import ObjectsFilter from "../../../components/objects/ObjectsFilter";
-    export default {
-        components: {ObjectsFilter, CrudList},
-        computed: {
-            actions() {
-              if(this.$store.state.authentication.user.roles.includes('ROLE_ADMIN')) {
-                  return ['edit', 'delete']
-              }
-              return ['edit']
-            },
-            fields() {
-                return [
-                    {key: 'title', label: 'Наименование', type: EditLink},
-                    {key: 'address', label: 'Адрес'},
-                    {key: 'city', label: 'Город'},
-                    {key: 'category', label: 'Категория'},
-                    {key: 'createdBy', label: 'Пользователь'},
-                    {key: 'createdAt', label: 'Дата создания', type: FormattedDate},
-                ]
-                .filter(i => {
-                   return  i.key === 'createdBy' ? this.$store.state.authentication.user.roles.includes('ROLE_ADMIN') : true
-                })
-            }
+import CrudList from "~/components/crud/CrudList";
+import FormattedDate from "~/components/crud/list-fields/FormattedDate";
+import EditLink from "~/components/crud/list-fields/EditLink";
+import ObjectsFilter from "../../../components/objects/ObjectsFilter";
+export default {
+    components: { ObjectsFilter, CrudList },
+    computed: {
+        actions() {
+            //   if(this.$store.state.authentication.user.roles.includes('ROLE_ADMIN')) {
+            //       return ['edit', 'delete']
+            //   }
+            return ["edit"];
+        },
+        fields() {
+            return [
+                { key: "title", label: "Наименование", type: EditLink },
+                { key: "address", label: "Адрес" },
+                { key: "city", label: "Город" },
+                { key: "category", label: "Категория" },
+                { key: "createdBy", label: "Пользователь" },
+                {
+                    key: "createdAt",
+                    label: "Дата создания",
+                    type: FormattedDate
+                }
+            ].filter(i => {
+                return i.key === "createdBy";
+                // ? this.$store.state.authentication.user.roles.includes(
+                //       "ROLE_ADMIN"
+                //   )
+                // : true;
+            });
         }
     }
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
