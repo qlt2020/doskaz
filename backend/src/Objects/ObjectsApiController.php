07755137fc55af11e8bbdbbf94eba6f11cb5f2ab
akbayk <akuderbekov@nu.edu.kz>
2021-10-08 11:58:25 +0600

    commit fix

--- backend/src/Objects/ObjectsApiController.php
+++ backend/src/Objects/ObjectsApiController.php
@@ -1016,6 +1016,7 @@ final class ObjectsApiController extends AbstractController
         ];
     }
 
+    
     /**
      * @Route(path="/statistic", methods={"GET"})
      * @param Request $request
