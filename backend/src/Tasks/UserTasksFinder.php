ee541dd7d306b9802e9d16eb6aa998f6cabe0a57
akbayk <akuderbekov@nu.edu.kz>
2021-10-18 12:10:54 +0600

    task #131

--- backend/src/Tasks/UserTasksFinder.php
+++ backend/src/Tasks/UserTasksFinder.php
@@ -4,6 +4,7 @@
 namespace App\Tasks;
 
 use Doctrine\DBAL\Connection;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class UserTasksFinder
 {
@@ -12,25 +13,31 @@ class UserTasksFinder
      * @var CurrentTaskDataProvider
      */
     private CurrentTaskDataProvider $currentTaskDataProvider;
+    private TranslatorInterface $translator;
 
-    public function __construct(Connection $connection, CurrentTaskDataProvider $currentTaskDataProvider)
+    public function __construct(Connection $connection, CurrentTaskDataProvider $currentTaskDataProvider, TranslatorInterface $translator)
     {
         $this->connection = $connection;
         $this->currentTaskDataProvider = $currentTaskDataProvider;
+        $this->translator = $translator;
     }
 
-    public function findForUser(int $userId, int $cityId, int $page, string $sort, int $perPage = 10): array
+    public function findForUser(int $userId, int $cityId, int $page, string $sort, string $lang, int $perPage = 10): array
     {
-        $currentTask = $this->currentTaskDataProvider->forUser($userId, $cityId);
+        $currentTask = $this->currentTaskDataProvider->forUser($userId, $cityId, $lang);
+
+        $profile = $this->translator->trans('Заполните профиль', [], 'attributes', $lang);
+        $verification = $this->translator->trans('Верифицируйте 1 объект', [], 'attributes', $lang);
+        $addition = $this->translator->trans('Добавьте 1 объект', [], 'attributes', $lang);
 
         $query = "
-               select completed_at, 'Заполните профиль' as type, 4 as points, users.created_at, 0 as priority
+               select completed_at, '$profile' as type, 4 as points, users.created_at, 0 as priority
                   from profile_completion_tasks
                   join users on users.id = profile_completion_tasks.user_id
                   where user_id = :userId
                   and completed_at IS NOT NULL
-               union all select completed_at, 'Добавьте 1 объект' as type, reward as points, created_at, 0 as priority from daily_tasks where user_id = :userId  and completed_at IS NOT NULL
-               union all select completed_at, 'Верифицируйте 1 объект' as type, reward as points, created_at, 0 as priority from daily_verification_tasks where user_id = :userId  and completed_at IS NOT NULL
+               union all select completed_at, '$addition' as type, reward as points, created_at, 0 as priority from daily_tasks where user_id = :userId  and completed_at IS NOT NULL
+               union all select completed_at, '$verification' as type, reward as points, created_at, 0 as priority from daily_verification_tasks where user_id = :userId  and completed_at IS NOT NULL
                union all select user_administration_tasks.created_at as completed_at, administration_tasks.name as type, user_administration_tasks.points, administration_tasks.created_at, 0 as priority from user_administration_tasks
                   join administration_tasks on user_administration_tasks.task_id = administration_tasks.id
                   where user_administration_tasks.user_id = :userId
