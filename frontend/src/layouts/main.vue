356679a4f050190dc5cdf2963494a149cc89fb52
Dosarkz <ashenov.e@gmail.com>
2021-10-20 18:19:34 +0300

    check map error

--- frontend/src/layouts/main.vue
+++ frontend/src/layouts/main.vue
@@ -1,5 +1,5 @@
-<template>
-  <div class="main-page" :class="{ opened: mobileOpened }">
+<template >
+  <div v-if="loaded" class="main-page" :class="{ opened: mobileOpened }">
     <IntroForm />
     <div class="main-page__map">
       <client-only>
@@ -213,6 +213,11 @@ export default {
   },
   created() {
     this.$nuxt.$on("mainPageMobOpened", this.mobileOpenedTrue);
+    let self = this;
+    self.$nextTick(function() {
+      self.loaded = true;
+    })
+
   },
   beforeDestroy() {
     this.$nuxt.$off("mainPageMobOpened");
