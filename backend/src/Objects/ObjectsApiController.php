8eed16395fe93fd2311c6948a6c30d891577ec66
user@WIN-7ETRKHB28SJ <user@WIN-7ETRKHB28SJ>
2022-11-07 21:25:06 +0600

    fix objects to pdf by lang

--- backend/src/Objects/ObjectsApiController.php
+++ backend/src/Objects/ObjectsApiController.php
@@ -615,7 +615,19 @@ final class ObjectsApiController extends AbstractController
      */
     public function pdf(Request $request, MapObject $mapObject, Client $client)
     {
-        $request = new URLRequest($this->params->get('app.frontend_url').'/'.$request->getLocale(). '/objects/pdf?id=' . $mapObject->id());
+        $acceptLanguages = ['ru','en','kz'];
+        $requestLang = $request->lang;
+        if(!isset($requestLang) || trim($requestLang) === '' || !in_array($requestLang, $acceptLanguages)){
+            $requestLang = 'ru';
+        }
+        $locale = '';
+        if ($requestLang !== 'ru'){
+            $locale = $requestLang;
+        }
+
+//        $request = new URLRequest($this->params->get('app.frontend_url').'/'.$request->getLocale(). '/objects/pdf?id=' . $mapObject->id());
+        $request = new URLRequest($this->params->get('app.frontend_url').'/'.$locale. '/objects/pdf?id=' . $mapObject->id());
+
         $request->setMargins([0, 0, 0, 0]);
         $request->setPaperSize(URLRequest::A4);
         $path = tempnam('/tmp', 'pdf');
